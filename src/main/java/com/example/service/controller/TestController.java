dabf821dfb14e1de5fae3a4dcf01e47945bbc4ef
Dennis von der Bey <dennis@vonderbey.eu>
2021-09-12 21:39:20 +0200

    remove test controller

--- src/main/java/com/example/service/controller/TestController.java
+++
@@ -1,9 +0,0 @@
-package com.example.service.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-
-}
